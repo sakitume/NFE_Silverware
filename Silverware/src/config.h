d21b6c29b4035f533f85b389185e95deaefabb5c
Donovan Mandap <donovan@coremechanic.com>
2020-10-07 21:38:03 -0700

    Bumped up yaw rate

--- Silverware/src/config.h
+++ Silverware/src/config.h
@@ -30,7 +30,7 @@
 // *************rate in deg/sec
 // *************for acro mode
 #define MAX_RATE 860.0          //Roll & Pitch axis
-#define MAX_RATEYAW 500.0       //Yaw axis (used in acro and leveling modes)
+#define MAX_RATEYAW 675.0       //Yaw axis (used in acro and leveling modes)
 
 // *************EXPO from 0.00 to 1.00 , 0 = no exp
 // *************positive = less sensitive near center 
